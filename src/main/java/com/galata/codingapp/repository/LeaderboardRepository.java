e5f4e294eecdc1d21261d523923e55928cf7e21d
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-10-18 20:59:10 +0300

    leaderboard repository finished

--- src/main/java/com/galata/codingapp/repository/LeaderboardRepository.java
+++ src/main/java/com/galata/codingapp/repository/LeaderboardRepository.java
@@ -1,4 +1,12 @@
 package com.galata.codingapp.repository;
 
-public class LeaderboardRepository {
+import com.galata.codingapp.model.Leaderboard;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface LeaderboardRepository extends JpaRepository<Leaderboard, Long> {
+    List<Leaderboard> findAllByOrderByScoreDesc();
+    Leaderboard findByUsername(String username);
 }
