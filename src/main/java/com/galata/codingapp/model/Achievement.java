6dd4f30473aaeb99caf2f328e05a5117aa18569a
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-10-18 20:47:55 +0300

    achievement repository finished

--- src/main/java/com/galata/codingapp/model/Achievement.java
+++ src/main/java/com/galata/codingapp/model/Achievement.java
@@ -13,7 +13,7 @@ public class Achievement {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(nullable = false)
     private String name;
--- src/main/java/com/galata/codingapp/repository/AchievementRepository.java
+++ src/main/java/com/galata/codingapp/repository/AchievementRepository.java
@@ -1,4 +1,12 @@
 package com.galata.codingapp.repository;
 
-public class AchievementRepository {
+import com.galata.codingapp.model.Achievement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface AchievementRepository extends JpaRepository<Achievement, Long> {
+    List<Achievement>findByUsername(String username);
+    List<Achievement>findByUserId(Long id);
 }
