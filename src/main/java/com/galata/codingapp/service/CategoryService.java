ff68eee931cc07d52e716dfd37af038c033141ce
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-10-19 18:19:38 +0300

    category service finished

--- src/main/java/com/galata/codingapp/service/CategoryService.java
+++ src/main/java/com/galata/codingapp/service/CategoryService.java
@@ -1,4 +1,48 @@
 package com.galata.codingapp.service;
 
+import com.galata.codingapp.model.Category;
+import com.galata.codingapp.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Optional <Category > getCategoryById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    public Category getCategoryByName(String name) {
+        return categoryRepository.findByName(name);
+    }
+
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Category updateCategory(Long id ,Category updatedCategory) {
+        return categoryRepository.findById(id)
+                .map(category -> {
+                    category.setName(updatedCategory.getName());
+                    return categoryRepository.save(category);
+                })
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+    }
+
+    public void deleteCategory(Long id) {
+        if (categoryRepository.existsById(id)) {
+            categoryRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Category not found");
+        }
+    }
 }
