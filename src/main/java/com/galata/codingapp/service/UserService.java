bf545d7511019450c4e52f3ea5d0d0638097f3a4
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-10-19 16:19:08 +0300

    Update UserService

--- src/main/java/com/galata/codingapp/service/UserService.java
+++ src/main/java/com/galata/codingapp/service/UserService.java
@@ -26,19 +26,33 @@ public class UserService {
     }
 
     public User updateUser(Long id, User updatedUser) {
-        Optional<User> existingUser = userRepository.findById(id);
-        if (existingUser.isPresent()) {
-            User user = existingUser.get();
-            user.setUsername(updatedUser.getUsername());
-            user.setEmail(updatedUser.getEmail());
-            user.setPassword(updatedUser.getPassword());
-            user.setScore(updatedUser.getScore());
-            user.setLevel(updatedUser.getLevel());
-            return userRepository.save(user);
-        } else {
-            throw new RuntimeException("User not found");
-        }
+        return userRepository.findById(id)
+                .map(user -> {
+                        user.setUsername(updatedUser.getUsername());
+                        user.setEmail(updatedUser.getEmail());
+                        user.setPassword(updatedUser.getPassword());
+                        user.setScore(updatedUser.getScore());
+                        user.setLevel(updatedUser.getLevel());
+                        return userRepository.save(user);
+                })
+                .orElseThrow(() -> new RuntimeException("User not found"));
     }
+
+//    updateUser function with if else
+//    public User updateUser(Long id, User updatedUser) {
+//        Optional<User> existingUser = userRepository.findById(id);
+//        if (existingUser.isPresent()) {
+//            User user = existingUser.get();
+//            user.setUsername(updatedUser.getUsername());
+//            user.setEmail(updatedUser.getEmail());
+//            user.setPassword(updatedUser.getPassword());
+//            user.setScore(updatedUser.getScore());
+//            user.setLevel(updatedUser.getLevel());
+//            return userRepository.save(user);
+//        } else {
+//            throw new RuntimeException("User not found");
+//        }
+//    }
 
     public void deleteUser(Long id) {
         if (userRepository.existsById(id)) {
