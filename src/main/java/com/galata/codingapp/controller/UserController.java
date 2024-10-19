d3319220cb3bd62c8fa851da5d2e0cb71712578b
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-10-19 20:46:15 +0300

    update user controller

--- src/main/java/com/galata/codingapp/controller/UserController.java
+++ src/main/java/com/galata/codingapp/controller/UserController.java
@@ -43,6 +43,7 @@ public class UserController {
        }
    }
 
+   @DeleteMapping("/{id}")
    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
        try{
            userService.deleteUser(id);
