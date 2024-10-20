d97d08f161a0b1738076aebd182ce015d51bbfe5
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-10-20 14:51:27 +0300

    Task controller finished

--- src/main/java/com/galata/codingapp/controller/TaskController.java
+++ src/main/java/com/galata/codingapp/controller/TaskController.java
@@ -1,4 +1,55 @@
 package com.galata.codingapp.controller;
 
+import com.galata.codingapp.model.Task;
+import com.galata.codingapp.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        Optional<Task> task = taskService.getTaskById(id);
+        return task.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        Task createdTask = taskService.createTask(task);
+        return ResponseEntity.ok(createdTask);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id , @RequestBody Task updatedTask) {
+        try{
+            Task task = taskService.updateTask(id , updatedTask);
+            return ResponseEntity.ok(task);
+        } catch (RuntimeException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        try {
+            taskService.deleteTask(id);
+            return ResponseEntity.noContent().build();
+        }catch (RuntimeException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
