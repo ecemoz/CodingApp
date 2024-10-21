ac7be8bd6df49cfdbf5466d64180efeb093660e3
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-10-21 20:32:54 +0300

    submission controller finished

--- src/main/java/com/galata/codingapp/controller/SubmissionController.java
+++ src/main/java/com/galata/codingapp/controller/SubmissionController.java
@@ -1,8 +1,55 @@
 package com.galata.codingapp.controller;
 
+import com.galata.codingapp.model.Submission;
+import com.galata.codingapp.service.SubmissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
 
-
-
+@RestController
+@RequestMapping("/submissions")
 public class SubmissionController {
 
+    @Autowired
+    private SubmissionService submissionService;
+
+    @GetMapping
+    public List<Submission> getAllSubmissions() {
+        return submissionService.getAllSubmissions();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Submission> getSubmissionById(@PathVariable Long id) {
+        Optional<Submission> submission = submissionService.getSubmissionById(id);
+        return submission.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Submission> createSubmission(@RequestBody Submission submission) {
+        Submission createdSubmission = submissionService.createSubmission(submission);
+        return ResponseEntity.ok(createdSubmission);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Submission> updateSubmission(@PathVariable Long id , @RequestBody Submission updatedsubmission) {
+        try {
+            Submission submission = submissionService.updateSubmission(id, updatedsubmission);
+            return ResponseEntity.ok(submission);
+        } catch (RuntimeException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSubmission(@PathVariable Long id) {
+        try {
+            submissionService.deleteSubmission(id);
+            return ResponseEntity.noContent().build();
+        } catch (RuntimeException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
