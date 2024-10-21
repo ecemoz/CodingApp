44b69aef67639628ba5d927e3968576b440d285e
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-10-21 19:36:48 +0300

    Achievement controller finished

--- src/main/java/com/galata/codingapp/controller/AchievementController.java
+++ src/main/java/com/galata/codingapp/controller/AchievementController.java
@@ -1,4 +1,55 @@
 package com.galata.codingapp.controller;
 
+import com.galata.codingapp.model.Achievement;
+import com.galata.codingapp.service.AchievementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/achievements")
 public class AchievementController {
+
+    @Autowired
+    private AchievementService achievementService;
+
+    @GetMapping
+    public List<Achievement> getAllAchievements() {
+        return achievementService.getAllAchievements();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Achievement> getAchievementById(@PathVariable Long id) {
+        Optional<Achievement> achievement = achievementService.getAchievementById(id);
+        return achievement.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Achievement> createAchievement( @RequestBody Achievement achievement) {
+        Achievement createdAchievement = achievementService.createAchievement(achievement);
+        return ResponseEntity.ok(createdAchievement);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Achievement> updateAchievement( @PathVariable Long id , @RequestBody Achievement updatedAchievement) {
+        try {
+            Achievement achievement = achievementService.updateAchievement(id, updatedAchievement);
+            return ResponseEntity.ok(achievement);
+        }catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Achievement> deleteAchievement(@PathVariable Long id) {
+        try {
+            achievementService.deleteAchievement(id);
+            return ResponseEntity.noContent().build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
